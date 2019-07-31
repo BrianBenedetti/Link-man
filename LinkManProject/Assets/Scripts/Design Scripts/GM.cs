caf21d8c46873d18f0b26b3418166749017f6b87
MamelloRaboroko <mamelloraboroko8.mr@gmail.com>
2019-08-01 00:46:33 +0200

    Added score, health & font

---
+++ LinkManProject/Assets/Scripts/Design Scripts/GM.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class GM : MonoBehaviour
+{
+    //public variables
+    public int score;
+    public int health;
+    public TextMesh scoreText;
+    public TextMesh healthText; 
+    public static GM instance = null;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z)) 
+        {
+            HealthLoss();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            HealthGain();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ScoreGain();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Exit();
+        }
+
+    }
+
+    public void HealthLoss()
+    {
+        health--;
+        healthText.text = " " + health;
+    }
+
+    public void HealthGain()
+    {
+        health++;
+        healthText.text = " " + health;
+    }
+
+    public void ScoreGain ()
+    {
+        score++;
+        scoreText.text = " " + score;
+    }
+
+    public void Exit ()
+    {
+        //are u sure you want to quit
+        Application.Quit();
+    }
+}
