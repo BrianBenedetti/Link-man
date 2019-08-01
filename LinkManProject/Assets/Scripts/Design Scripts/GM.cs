9d162bc302d62a5ecac087b97c77e8b7da70920d
MamelloRaboroko <mamelloraboroko8.mr@gmail.com>
2019-08-01 13:36:24 +0200

    UI Complete. Just have to make it look pretty

--- LinkManProject/Assets/Scripts/Design Scripts/GM.cs
+++ LinkManProject/Assets/Scripts/Design Scripts/GM.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GM : MonoBehaviour
 {
@@ -9,7 +10,16 @@ public class GM : MonoBehaviour
     public int score;
     public int health;
     public TextMesh scoreText;
-    public TextMesh healthText; 
+    public TextMesh healthText;
+    public bool paused = false;
+    public GameObject pausedText;
+    public GameObject WinText;
+    public GameObject gameOverText;
+    public GameObject restartText;
+    public GameObject quitMenu;
+    public GameObject cursor;
+    public Transform yesText;
+    public Transform noText;
     public static GM instance = null;
 
 
@@ -30,24 +40,37 @@ public class GM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z)) 
+        if (Input.GetKeyDown(KeyCode.Z)) //stand in UI interactions
         {
             HealthLoss();
         }
 
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X))//stand in UI interactions
         {
             HealthGain();
         }
 
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C))//stand in UI interactions
         {
             ScoreGain();
         }
 
+        PauseGame();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            restartText.SetActive(true);
+            Invoke("ReloadGame", 1.5f);
+        }
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Exit();
+            ActivateQuitMenu();
+        }
+
+        if (quitMenu.activeSelf == true)
+        {
+            QuitChoice();
         }
 
     }
@@ -56,6 +79,12 @@ public class GM : MonoBehaviour
     {
         health--;
         healthText.text = " " + health;
+        
+        //losing
+        if (health == 0)
+        {
+            gameOverText.SetActive(true);
+        }
     }
 
     public void HealthGain()
@@ -68,11 +97,70 @@ public class GM : MonoBehaviour
     {
         score++;
         scoreText.text = " " + score;
+
+        //winning
+        if (score == 20) //20 is random number for testing
+        {
+            WinText.SetActive(true);
+        }
     }
 
-    public void Exit ()
+    void PauseGame()
     {
-        //are u sure you want to quit
-        Application.Quit();
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            //"!" inverts boolean so if paused is true "!" inverts it to false. if pased is faalse, "!" inverts it to true. (thats why can press same button for pause & resume)
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            Time.timeScale = 0f;
+            pausedText.SetActive(true);
+        }
+
+        else if (!paused)
+        {
+            Time.timeScale = 1f;
+            pausedText.SetActive(false);
+        }
+    }
+
+    void ActivateQuitMenu ()
+    {
+        quitMenu.SetActive(true);
+    }
+
+    void ReloadGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+    }
+
+    void QuitChoice ()
+    {
+        //seclecting yes to quit
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            cursor.transform.position = noText.position;
+        }
+
+        //seclecting no to quit
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            cursor.transform.position = yesText.position;
+        }
+
+        //not quiting
+        if (cursor.transform.position == noText.transform.position && Input.GetKeyDown(KeyCode.Space))
+        {
+            quitMenu.SetActive(false);
+        }
+
+        //confirming quiting
+        if (cursor.transform.position == yesText.transform.position && Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Application.Quit();
+            print("quit");
+        }
     }
 }
