6f938a72685dc22ae28aacbc39301bc1703e91f9
FalseCrystal <kakitleong8@gmail.com>
2019-08-05 09:04:38 +0200

    Updated final Integration scene with working Saatwiks test scene

---
+++ LinkManProject/Assets/Scripts/Restart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+
+    Vector3 startpos;
+
+    public void death() {
+
+        GetComponent<Movement>().enabled = false;
+
+    }
+
+    public void restart() {
+
+        transform.position = new Vector3 (7f,8f,0f);
+
+
+        GetComponent<Movement>().enabled = true;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        startpos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
